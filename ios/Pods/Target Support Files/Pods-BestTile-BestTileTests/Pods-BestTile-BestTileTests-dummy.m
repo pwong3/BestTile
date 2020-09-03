30d97442e4a08a54f103ca59aa09fd3649f83d1c
pwong3 <patrickycwong@gmail.com>
2020-09-03 14:27:00 -0700

    Initial commit

---
+++ ios/Pods/Target Support Files/Pods-BestTile-BestTileTests/Pods-BestTile-BestTileTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BestTile_BestTileTests : NSObject
+@end
+@implementation PodsDummy_Pods_BestTile_BestTileTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-BestTile-tvOSTests/Pods-BestTile-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BestTile_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_BestTile_tvOSTests
+@end
